9b70846e0e6ba195e40a9bcdcb40714dd5cda8c2
Mattie Zilaie <your_name@domain.comAccount>
2019-10-15 14:53:34 -0700

    fall distance using function

---
+++ fall_distance.py
@@ -0,0 +1,8 @@
+# Author: Mahtab Zilaie
+# Date:10-15-19
+# Description: get fall distance using a function
+
+
+def fall_distance(t):
+    """function returns fall distance of object due to gravity"""
+    return float((1/2) * 9.8 * t**2)
